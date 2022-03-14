d44007b2697433f2493a42b5e644547c6aff9512
Papouete <flo.mor97@gmail.com>
2022-03-14 15:12:53 +0100

    exo3

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 //const helloWorld = React.createElement('h1', {}, 'Hello world!');
 
-const helloWorld = <h1>Hello world!</h1>;
+const lastName = 'moreau';
+const firstName = 'florian';
 
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function toUpperCase(string) {
+    return string.toUpperCase();
+}
+
+const element = (
+    <div>
+        <span>Bonjour </span>
+        <span class ="red">{toUpperCase(lastName)} </span>
+        <span>{capitalizeFirstLetter(firstName)} !</span>
+    </div>
+)
+
+ReactDOM.render(
+    element,
+    document.querySelector('#app')
+    );
